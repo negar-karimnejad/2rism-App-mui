3cb84afea32341e2bc0b68fc431556689e4f5f3d
negar <negar.karimnejad@gmail.com>
2024-01-13 13:26:46 +0330

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { routes } from "./routes";
+import { useRoutes } from "react-router-dom";
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import Topbar from "./pages/global/Topbar";
+import Sidebar from "./pages/global/Sidebar";
+import Dashboard from "./pages/dashboard";
+import Team from "./pages/team";
+import Invoices from "./pages/invoices";
+import Contacts from "./pages/contacts";
+import Bar from "./pages/bar";
+import Form from "./pages/form";
+import Line from "./pages/line";
+import Pie from "./pages/pie";
+import FAQ from "./pages/faq";
+import Geography from "./pages/geography";
+
+function App() {
+  const router = useRoutes(routes);
+  const [theme, colorMode] = useMode();
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content">
+            <Topbar />
+            <Sidebar />
+            {router}
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+}
+
+export default App;
---
+++ src/routes.jsx
@@ -0,0 +1,4 @@
+
+export const routes = [
+    // {path:"/",element:<App/>}
+]
